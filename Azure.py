a5fb523c35e294366e4e062516a141a0e5baba01
Bavly Awad <67879620+Bavly-Awad@users.noreply.github.com>
2022-11-22 13:55:18 +0000

    Added the code of the Azure Secure Login for beginners.

---
+++ Azure.py
@@ -0,0 +1,18 @@
+print("Azure Secure Login")
+
+clientinput = input("Username > ")
+clientpass = input("Password > ")
+# Create a secure connection to Azure
+
+# Creating VIP outputs for clients of azure.
+
+if clientinput == "bavly" and clientpass == "codinglesson":
+    print("Welcome to the azure dashboard", clientinput)
+
+elif clientinput == "malcolm" and clientpass == "bavlyisgoated":
+    print("Welcome to the azure dashobaord", clientinput)
+
+elif clientinput == "nick" and clientpass == "thanksbro":
+    print("Welcome to the azure dashboard", clientinput)
+else:
+    print("You do not have access..")
